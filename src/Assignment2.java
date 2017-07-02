14075f1e6ad82ecbd5242a90164f424c6e30da7e
SAI-ANIRUDH <anirudh@saisam>
2017-07-02 11:59:16 +0530

    Session1 - Assignment2 Program

---
+++ src/Assignment2.java
@@ -0,0 +1,11 @@
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 4; // * 3 = 0000 0101//
+int b = 1; // * 1 = 0000 0001//
+int c= a | b;
+System.out.println(""+c);
+	}
+
+}
